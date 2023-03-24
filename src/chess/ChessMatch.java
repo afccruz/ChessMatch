fc51c9aeaa4f579590a8abbff0e0ec797a3500f3
Andre Cruz <afcruz@primeit.pt>
2023-03-24 12:08:56 +0000

    Added ChessException; Added place piece movement by user readable form; Added ChessPosition to covert the matrix to human level and vice-versa; Little improvements in the board printing;

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.enums.Color;
 import chess.pieces.King;
 import chess.pieces.Rook;
@@ -19,6 +18,7 @@ public class ChessMatch {
 
     /**
      * Get the pieces of each position to print
+     *
      * @return matrix of pieces
      */
     public ChessPiece[][] getPieces() {
@@ -36,9 +36,29 @@ public class ChessMatch {
     /**
      * Creates the pieces in the initial position
      */
-    private void initialSetup(){
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0 ,4));
-        board.placePiece(new King(board, Color.WHITE), new Position(7 ,4));
+    private void initialSetup() {
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
+    }
+
+    /**
+     * Place the piece on the board convert the human-readable position to the matrix position
+     *
+     * @param column
+     * @param row
+     * @param piece
+     */
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
     }
 }
