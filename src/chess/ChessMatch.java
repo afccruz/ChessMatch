4c006afe6846eae4e2900717426dde45a61e4af7
Andre Cruz <afcruz@primeit.pt>
2023-03-27 16:11:38 +0100

    Pawn piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -5,6 +5,7 @@ import boardgame.Piece;
 import boardgame.Position;
 import chess.enums.Color;
 import chess.pieces.King;
+import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
 import java.util.ArrayList;
@@ -70,26 +71,29 @@ public class ChessMatch {
      * Creates the pieces in the initial position
      */
     private void initialSetup() {
-//        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
-//        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
-//        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
-//        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
-//        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
-//        placeNewPiece('d', 1, new King(board, Color.WHITE));
-//
-//        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
-//        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
-//        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
-//        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
-//        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
-//        placeNewPiece('d', 8, new King(board, Color.BLACK));
-
-        placeNewPiece('h', 7, new Rook(board, Color.WHITE));
-        placeNewPiece('d', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('a', 1, new Rook(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('h', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('c', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('d', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('e', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('f', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
-        placeNewPiece('b', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('a', 8, new King(board, Color.BLACK));
+        placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('h', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('c', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('d', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('e', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('f', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('g', 7, new Pawn(board, Color.BLACK));
+        placeNewPiece('h', 7, new Pawn(board, Color.BLACK));
 
     }
 
@@ -141,10 +145,13 @@ public class ChessMatch {
      * @return
      */
     private Piece makeMove(Position source, Position target) {
-        Piece p = board.removePiece(source);
+        ChessPiece p = (ChessPiece) board.removePiece(source);
+        p.increaseMoveCount();
+
         Piece capturedPiece = board.removePiece(target);
 
         board.placePiece(p, target);
+
         if (capturedPiece != null) {
             piecesOnTheBoard.remove(capturedPiece);
             this.capturedPieces.add(capturedPiece);
@@ -161,7 +168,9 @@ public class ChessMatch {
      * @param capturedPiece
      */
     private void undoMove(Position source, Position target, Piece capturedPiece) {
-        Piece p = board.removePiece(target);
+        ChessPiece p = (ChessPiece) board.removePiece(target);
+        p.decreaseMoveCount();
+
         board.placePiece(p, source);
 
         if (capturedPiece != null) {
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,6 +7,7 @@ import chess.enums.Color;
 
 public abstract class ChessPiece extends Piece {
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
@@ -15,6 +16,10 @@ public abstract class ChessPiece extends Piece {
 
     public Color getColor() {
         return color;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
     }
 
     @Override
@@ -30,5 +35,13 @@ public abstract class ChessPiece extends Piece {
 
     public ChessPosition getChessPosition() {
         return ChessPosition.fromPosition(position);
+    }
+
+    public void increaseMoveCount(){
+        this.moveCount++;
+    }
+
+    public void decreaseMoveCount(){
+        this.moveCount--;
     }
 }
